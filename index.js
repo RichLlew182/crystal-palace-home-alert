4b1ebb0981215e08d40d5ec70f05358602309f0b
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-02-25 13:45:06 +0000

    added relativeTime extension to dayjs

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const dayjs = require('dayjs')
+const dayjs = require('dayjs');
+var relativeTime = require('dayjs/plugin/relativeTime')
+dayjs.extend(relativeTime)
 
-const url = 'https://api-football-v1.p.rapidapi.com/v3/fixtures?league=39&team=52&next=50';
+var timeFromNow = dayjs().to(dayjs('2024-02-29'))
+console.log(timeFromNow);
+
+const url = 'https://api-football-v1.p.rapidapi.com/v3/fixtures?team=52&next=50';
 const options = {
 	method: 'GET',
 	headers: {
@@ -9,25 +14,35 @@ const options = {
 	}
 };
 
-fetch(url, options)
-  .then(function (result) {
-    return result.json();
-  }).then(function (data) {
-
-    // console.log(data.response[0])
-
-    const crystalPalaceFixtures = data.response
-
-    for (let i = 0; i < crystalPalaceFixtures.length; i++) {
-      const venue = crystalPalaceFixtures[i].fixture.venue.name;
-      const homeTeam = crystalPalaceFixtures[i].teams.home.name;
-      const awayTeam = crystalPalaceFixtures[i].teams.away.name;
-      let date = crystalPalaceFixtures[i].fixture.date
-      date = dayjs(date).format('DD/MM/YY')
-      console.log('------------------------------------------------------------')
-      console.log(`${homeTeam} are playing ${awayTeam} at ${venue} on ${date}`)
-    }
+// fetch(url, options)
+//   .then(function (result) {
+//     return result.json();
+//   }).then(function (data) {
 
     
-  })
+//     const crystalPalaceFixtures = data.response
+    
+//     for (let i = 0; i < crystalPalaceFixtures.length; i++) {
+//       console.log(data.response[i])
+//       const venue = crystalPalaceFixtures[i].fixture.venue.name;
+//       const homeTeam = crystalPalaceFixtures[i].teams.home.name;
+//       const awayTeam = crystalPalaceFixtures[i].teams.away.name;
+//       const league = crystalPalaceFixtures[i].league.name;
+//       let date = crystalPalaceFixtures[i].fixture.date
+//       date = dayjs(date).format('DD/MM/YY')
+      
+//       console.log('------------------------------------------------------------')
+//       console.log(`${homeTeam} are playing ${awayTeam} in the ${league} at ${venue} on ${date}`)
+
+//       if (date)
+
+
+//     }
+
+    
+//   })
+//   .catch(function (error) {
+//     // Handle errors
+//     console.error('Error:', error);
+//   });
 
