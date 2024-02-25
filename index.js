62ab7a49c611ef8d8ce5e64e191782227b84cff9
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-02-25 15:11:01 +0000

    used the diff method on day js to get the number of days away the next game is from today

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ dayjs.extend(relativeTime)
 var timeFromNow = dayjs().to(dayjs('2024-02-29'))
 console.log(timeFromNow);
 
+let todaysDate = dayjs()
+// console.log(todaysDate)
+
 const url = 'https://api-football-v1.p.rapidapi.com/v3/fixtures?team=52&next=50';
 const options = {
 	method: 'GET',
@@ -14,35 +17,35 @@ const options = {
 	}
 };
 
-// fetch(url, options)
-//   .then(function (result) {
-//     return result.json();
-//   }).then(function (data) {
+fetch(url, options)
+  .then(function (result) {
+    return result.json();
+  }).then(function (data) {
 
     
-//     const crystalPalaceFixtures = data.response
+    const crystalPalaceFixtures = data.response
     
-//     for (let i = 0; i < crystalPalaceFixtures.length; i++) {
-//       console.log(data.response[i])
-//       const venue = crystalPalaceFixtures[i].fixture.venue.name;
-//       const homeTeam = crystalPalaceFixtures[i].teams.home.name;
-//       const awayTeam = crystalPalaceFixtures[i].teams.away.name;
-//       const league = crystalPalaceFixtures[i].league.name;
-//       let date = crystalPalaceFixtures[i].fixture.date
-//       date = dayjs(date).format('DD/MM/YY')
+    for (let i = 0; i < crystalPalaceFixtures.length; i++) {
+      // console.log(data.response[i])
+      const venue = crystalPalaceFixtures[i].fixture.venue.name;
+      const homeTeam = crystalPalaceFixtures[i].teams.home.name;
+      const awayTeam = crystalPalaceFixtures[i].teams.away.name;
+      const league = crystalPalaceFixtures[i].league.name;
+      let futureDate = dayjs(crystalPalaceFixtures[i].fixture.date);
+      let daysFromNow = futureDate.diff(todaysDate, 'day');
+      // let daysFromNow = futureDate.diff(dayjs(), 'days')
+      // console.log(daysFromNow)
       
-//       console.log('------------------------------------------------------------')
-//       console.log(`${homeTeam} are playing ${awayTeam} in the ${league} at ${venue} on ${date}`)
-
-//       if (date)
+      console.log('------------------------------------------------------------')
+      console.log(`${homeTeam} are playing ${awayTeam} in the ${league} at ${venue} on ${futureDate} which is ${daysFromNow} days away.`)
 
 
-//     }
+    }
 
     
-//   })
-//   .catch(function (error) {
-//     // Handle errors
-//     console.error('Error:', error);
-//   });
+  })
+  .catch(function (error) {
+    // Handle errors
+    console.error('Error:', error);
+  });
 
