c5c636c1c2e8b5724c030a12e7e7931d9d60618f
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-02-25 15:47:50 +0000

    uninstalled nodemailer and removed function related to it

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const dayjs = require('dayjs');
-const nodemailer = require('nodemailer');
 
 let todaysDate = dayjs()
 // console.log(todaysDate)
@@ -12,34 +11,6 @@ const options = {
 		'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
 	}
 };
-
-// Function to send alert email
-function sendAlertEmail() {
-  // Create a SMTP transporter
-  let transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-          user: 'richllewdesign@gmail.com', // Your email address
-          pass: 'RichLlewDesign182' // Your password
-      }
-  });
-
-  // Setup email data
-  let mailOptions = {
-      from: 'richllewdesign@gmail.com',
-      to: 'richard.lee.llewellyn@gmail.com',
-      subject: 'Alert: Crystal Palace are at home this week!',
-      text: 'Sainsburys will be closed!'
-  };
-
-  // Send email
-  transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          return console.log('Error occurred while sending email:', error);
-      }
-      console.log('Email sent:', info.response);
-  });
-}
 
 fetch(url, options)
   .then(function (result) {
